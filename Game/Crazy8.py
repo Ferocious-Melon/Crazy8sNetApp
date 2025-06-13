d8e2f900307826845ecf31d9dca626a1aab20945
Wil <148395263+wilbert849@users.noreply.github.com>
2025-06-13 11:26:35 -0400

    Update Crazy8.py

--- Game/Crazy8.py
+++ Game/Crazy8.py
@@ -126,7 +126,7 @@ def game(): #Main game
             #While there are still cards to play...
             while(len(possibleCards) != 0):
                 #Ask player to pick a card to play or void the rest of their turn
-                choice = int(grabInput("Choose an index to play or another input to continue")) 
+                choice = int(grabInput("Choose an index to play or another input to continue: ")) 
                 
                 if choice in possibleCards and cardInPlay.compare(currentHand[choice]): #If valid choice
                     cardInPlay = currentHand.pop(choice)    #Play the card
